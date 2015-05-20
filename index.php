3a1458a2b7faf6c351b1616e5c7e70492c1baaf0
Shawn Thompson <plansmash@me.com>
2015-05-20 17:33:39 -0400

    initial commit original site

--- index.php
+++ index.php
@@ -1,17 +1,47 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+	<title>Artsida.org</title>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+	<!-- Bootstrap -->
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Fav Icon -->
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<link href="favicon.ico" rel="shortcut icon">
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+  </head>
+  <body class="container">
+	<h1>Artsida.org</h1>
+	<div class="col-xs-5 col-sm-2 col-sm-push-5"><img src="artsida-logo.jpg" class="img-responsive" alt=""></div>
+	<div class="clearfix"></div>
+	<div class="row">
+		<section class="col-md-6">
+			<h2>Artsida 6: Raise Your Hand for <abbr title="AIDS Community Care Montreal">ACCM</abbr></h2>
+			<p>Artsida, ACCM’s signature art auction, returns for its much-anticipated sixth edition on <strong>Wednesday, September 30th</strong>. Artsida has become an important event on the social calendar for art admirers and philanthropists who wish to make a difference in the community. Held at the stunningly restored Entrepôts Dominion, this year’s remodeled event will take new directions that will solidify Artsida’s position as an essential pillar of Montreal’s cultural circuit. Outstanding works from leading and emerging artists will not only inspire you to connect with captivating art but also to our compelling cause.</p>
+			<p>Exciting prizes will also be available for auction, such as two WestJet round trip tickets to anywhere in North America! Stay tuned for our forthcoming re-vamped website with photos of the artwork to be auctioned and more news about prizes.</p>
+			<p>Looking forward to seeing you in September!</p>
+			<p class="text-center"><a href="http://accmontreal.org/"><img class="img-responsive" src="accm-en.jpg" alt="ACCM: AIDS Community Care Montreal"></a></p>
+		</section>
+		<section class="col-md-6">
+			<h2>Artsida 6: Levez votre main pour <abbr title="SIDA Bénévoles Montréal">ACCM</abbr></h2>
+			<p>Artsida, l’événement phare d'ACCM — Sida Bénévoles Montréal, revient pour une sixième édition très attendue le <strong>mercredi 30 septembre</strong>. Artsida est devenu un événement important à l'agenda des amateurs d'art et des philanthropes soucieux de faire une différence dans la communauté. Tenu aux Entrepôts Dominion, un espace élégamment restauré, Artsida offrira aussi une formule repensée en adoptant de nouvelles directions qui consolideront sa position comme pilier essentiel du circuit culturel montréalais. Des oeuvres exceptionnelles d’artistes renommés et émergents ne feront pas que vous inciter à entrer en contact avec de l'art captivant, mais vous inspireront également à prendre part à notre cause incontournable.</p>
+			<p>Des prix excitants seront aussi offerts en encan, tels que deux billets aller-retour de WestJet pour n'importe quelle destination en Amérique du Nord! Restez à l'affût pour le lancement de notre nouveau site web qui inclura le catalogue des œuvres ainsi que les annonces d’autres prix.</p>
+			<p>Au plaisir de vous voir tous en septembre!</p>
+			<p class="text-center"><a href="http://accmontreal.org/fr/"><img class="img-responsive" src="accm-fr.jpg" alt="ACCM: SIDA Bénévoles Montréal"></a></p>
+		</section>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  </body>
+</html>
