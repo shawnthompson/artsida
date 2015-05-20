6a574dc3fcb5943c2a58ef61083b417d03a606e3
Shawn Thompson <plansmash@me.com>
2015-05-20 17:36:47 -0400

    initial static site commit

---
+++ index.old.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>ArtSIDA.org</title>
+
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/theme.old.css">
+
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body class="row">
+		<header class="row">
+			<div class="row eqht clearfix">
+				<div class="col-md-8 span clearfix">
+					<h1>ArtSIDA <span>6</span></h1>
+					<h2>Raise your hand for <abbr title="AIDS Community Care Montreal">ACCM</abbr></h2>
+					<div class="col-md-6 col-md-push-4">
+						<div class="col-md-6" id="logo"><img class="img-responsive" src="img/logo.jpg" alt="ArtSIDA logo"></div>
+						<div class="col-md-5" id="header-sponsor">
+							<p>Presented by: <img class="img-responsive" src="img/header-sponsor.png" alt=""></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 span clearfix red">
+					<div class="col-md-12 text-right yellow">
+						<section>
+							<h2 class="sr-only">Language selection</h2>
+							<ul class="list-unstyled">
+								<li class="text-right"><a lang="en" href="/fr/index.html">Fr<span class="sr-only">ançais</span></a></li>
+							</ul>
+						</section>
+						<p><time>Wednesday September 30, 2015</time></p>
+						<p>Entrepôts Dominion</p>
+						<p>3968 St-Ambroise</p>
+						<p>5:00pm - 10:30pm</p>
+					</div>
+					<div class="col-md-12 red">
+						<div class="col-md-8">
+							<img src="img/pic1.jpg" class="img-responsive" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<main>
+			<div class="row">
+				<div class="col-md-5">
+					<div class="col-md-12">
+						<div class="col-md-8 yellow">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+						</div>
+						<div class="col-md-4 cyan">
+							
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="col-md-2 red box">
+							
+						</div>
+						<div class="col-md-10 purple">
+							<p><a class="nav-link" href="#">About</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-7 grey">
+					<img src="img/art1.jpg" class="img-responsive" alt="">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-2 yellow">
+					
+				</div>
+				<div class="col-md-4 pink">
+					<p><a class="nav-link" href="#">Tickets</a></p>
+				</div>
+				<div class="col-md-6 red">
+					<img src="img/pic2.jpg" class="img-responsive" alt="">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-2">
+					<img src="img/pic3.jpg" class="img-responsive" alt="">
+				</div>
+				<div class="col-md-10">
+					<div class="row">
+						<div class="col-md-4 purple">
+						</div>
+						<div class="col-md-6 cyan">
+							<p><a class="nav-link" href="#">Artwork</a></p>
+						</div>
+						<div class="col-md-2 yellow">
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/jquery.equalheights.min.js"></script>
+	<script>
+	setTimeout(function(){ $('.eqht .span').equalHeights(); }, 300);
+		
+	</script>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,47 +1,165 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<title>Artsida.org</title>
-
-	<!-- Bootstrap -->
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
-
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>ArtSIDA.org</title>
 	<!-- Fav Icon -->
-	<link rel="icon" href="favicon.ico" type="image/x-icon">
+		<link rel="icon" href="favicon.ico" type="image/x-icon">
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<link href="favicon.ico" rel="shortcut icon">
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-  </head>
-  <body class="container">
-	<h1>Artsida.org</h1>
-	<div class="col-xs-5 col-sm-2 col-sm-push-5"><img src="artsida-logo.jpg" class="img-responsive" alt=""></div>
-	<div class="clearfix"></div>
-	<div class="row">
-		<section class="col-md-6">
-			<h2>Artsida 6: Raise Your Hand for <abbr title="AIDS Community Care Montreal">ACCM</abbr></h2>
-			<p>Artsida, ACCM’s signature art auction, returns for its much-anticipated sixth edition on <strong>Wednesday, September 30th</strong>. Artsida has become an important event on the social calendar for art admirers and philanthropists who wish to make a difference in the community. Held at the stunningly restored Entrepôts Dominion, this year’s remodeled event will take new directions that will solidify Artsida’s position as an essential pillar of Montreal’s cultural circuit. Outstanding works from leading and emerging artists will not only inspire you to connect with captivating art but also to our compelling cause.</p>
-			<p>Exciting prizes will also be available for auction, such as two WestJet round trip tickets to anywhere in North America! Stay tuned for our forthcoming re-vamped website with photos of the artwork to be auctioned and more news about prizes.</p>
-			<p>Looking forward to seeing you in September!</p>
-			<p class="text-center"><a href="http://accmontreal.org/"><img class="img-responsive" src="accm-en.jpg" alt="ACCM: AIDS Community Care Montreal"></a></p>
-		</section>
-		<section class="col-md-6">
-			<h2>Artsida 6: Levez votre main pour <abbr title="SIDA Bénévoles Montréal">ACCM</abbr></h2>
-			<p>Artsida, l’événement phare d'ACCM — Sida Bénévoles Montréal, revient pour une sixième édition très attendue le <strong>mercredi 30 septembre</strong>. Artsida est devenu un événement important à l'agenda des amateurs d'art et des philanthropes soucieux de faire une différence dans la communauté. Tenu aux Entrepôts Dominion, un espace élégamment restauré, Artsida offrira aussi une formule repensée en adoptant de nouvelles directions qui consolideront sa position comme pilier essentiel du circuit culturel montréalais. Des oeuvres exceptionnelles d’artistes renommés et émergents ne feront pas que vous inciter à entrer en contact avec de l'art captivant, mais vous inspireront également à prendre part à notre cause incontournable.</p>
-			<p>Des prix excitants seront aussi offerts en encan, tels que deux billets aller-retour de WestJet pour n'importe quelle destination en Amérique du Nord! Restez à l'affût pour le lancement de notre nouveau site web qui inclura le catalogue des œuvres ainsi que les annonces d’autres prix.</p>
-			<p>Au plaisir de vous voir tous en septembre!</p>
-			<p class="text-center"><a href="http://accmontreal.org/fr/"><img class="img-responsive" src="accm-fr.jpg" alt="ACCM: SIDA Bénévoles Montréal"></a></p>
-		</section>
-	</div>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-  </body>
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/theme.css">
+
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body class="home">
+		<header>
+			<div class="col-md-8 row2 left-col">
+				<h1><span class="sr-only">ArtSIDA 6</span><img src="img/heading.jpg" class="center-block img-responsive" alt="" /></h1>
+				<h2>Raise your hand for <abbr title="AIDS Community Care Montreal">ACCM</abbr></h2>
+				<div class="col-xs-6 col-xs-push-4 logos">
+					<div class="col-xs-6 col-sm-4"><img class="img-responsive" src="img/logo.gif" alt="ArtSIDA logo"></div>
+					<div class="col-xs-6 col-sm-3" id="header-sponsor">
+						<p>Presented by: <img class="img-responsive" src="img/header-sponsor.gif" alt="TD logo"></p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 row2 right-col">
+				<div class="col-md-12 text-right yellow info">
+					<section class="col-xs-11">
+						<h2 class="sr-only">Site Menu</h2>
+						<ul class="nav nav-pills">
+						  <li role="presentation" class="dropdown">
+						    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">
+						      Menu <span class="caret"></span>
+						    </a>
+						    <ul class="dropdown-menu" role="menu">
+						    	<li><a href="#">About</a></li>
+						    	<li><a href="#">Tickets</a></li>
+						    	<li><a href="#">Artwork</a></li>
+						    	<li><a href="#">Sponsors</a></li>
+						    </ul>
+						  </li>
+						</ul>
+					</section>
+					<section class="col-xs-1">
+						<h2 class="sr-only">Language selection</h2>
+						<ul class="nav nav-pills">
+							<li class="text-right"><a lang="en" href="#" class="lang">Fr<span class="sr-only">ançais</span></a></li>
+						</ul>
+					</section>
+					<div class="clearfix"></div>
+					<p><time>Wednesday September 30, 2015</time></p>
+					<p><span class="sr-only">Entrepôts Dominion</span><img src="img/logo-dominion.png" alt=""></p>
+					<p>3968 St-Ambroise</p>
+					<p>5:00pm - 10:30pm</p>
+				</div>
+				<div class="col-md-12 red row1">
+						<img src="img/pic1.jpg" class="img-responsive" alt="">
+				</div>
+			</div>
+		</div>
+		</header>
+		<div class="clearfix"></div>
+		<main>
+
+
+<div class="col-md-8">
+<div class="row1 col-md-8 yellow">
+</div>
+<div class="row1 col-md-4 cyan">
+</div>
+<div class="row1 col-md-2 red box">
+</div>
+<div class="row1 col-md-10 purple">
+	<p><a class="nav-link" href="#">About</a></p>
+</div>
+</div>
+<div class="row2 col-md-4 grey">
+	<img src="img/art1.jpg" class="img-responsive" alt="">
+</div>
+
+
+
+<div class="col-md-2 yellow row1">
+</div>
+<div class="col-md-4 pink row1">
+	<p><a class="nav-link" href="#">Tickets</a></p>
+</div>
+<div class="col-md-3 red row1">
+</div>
+<div class="col-md-3 row1 cyan">
+	<img src="img/pic2.jpg" class="img-responsive" alt="">
+</div>
+
+
+
+
+
+<div class="col-md-2 row2">
+	<img src="img/pic3.jpg" class="img-responsive" alt="">
+</div>
+<div class="col-md-2 purple row1">
+</div>
+<div class="col-md-6 cyan row1">
+	<p><a class="nav-link" href="#">Artwork</a></p>
+</div>
+<div class="col-md-2 yellow row1">
+</div>
+
+
+
+
+
+
+
+
+			<div class="clearfix"></div>
+		</main>
+		<footer class="col-xs-12">
+			<div class="col-md-4 col-md-push-8">
+				<section class="col-xs-6 col-md-10 text-right">
+					<h2 class="sr-only">Social Media</h2>
+					<ul class="list-inline">
+						<li><a href="#"><i class="fa fa-facebook-square fa-4x"></i><span class="sr-only">Facebook</span></a></li>
+						<li><a href="#"><i class="fa fa-twitter-square fa-4x"></i><span class="sr-only">Twitter</span></a></li>
+						<li><a href="#"><i class="fa fa-instagram fa-4x"></i><span class="sr-only">Instagram</span></a></li>
+						<li><a href="#"><i class="fa fa-tumblr-square fa-4x"></i><span class="sr-only">Tumblr</span></a></li>
+					</ul>
+				</section>
+				<section class="col-xs-6 col-md-2 text-right">
+					<h2 class="sr-only">Information links</h2>
+					<ul class="list-unstyled">
+						<li><a href="#">Media</a></li>
+						<li><a href="#">Volunteer</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</section>
+			</div>
+			<section class="col-xs-12 col-md-8 col-md-pull-4">
+				<h2 class="sr-only">Sponsors</h2>
+				<ul class="list-inline">
+					<li class="col-xs-3"><a href="#"><img class="img-responsive" src="img/sponsor1.jpg" alt=""></a></li>
+					<li class="col-xs-3"><a href="#"><img class="img-responsive" src="img/sponsor2.jpg" alt=""></a></li>
+					<li class="col-xs-3"><a href="#"><img class="img-responsive" src="img/sponsor3.jpg" alt=""></a></li>
+					<li class="col-xs-3"><a href="#"><img class="img-responsive" src="img/sponsor4.jpg" alt=""></a></li>
+				</ul>
+			</section>
+		</footer>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	</body>
 </html>
