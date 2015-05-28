ec4f734e2fc431582b664033af7f10eb96be8e12
Shawn Thompson <plansmash@me.com>
2015-05-28 17:56:06 -0400

    added the image for vernissage page

--- about-vernissage.php
+++ about-vernissage.php
@@ -1,7 +1,7 @@
 <?php
 	$title = "Artsida 6 Vernissage & Art Sale";
 	$pageClass = "content";
-	$mainImage ="about-accm.jpg";
+	$mainImage ="about-vernissage.jpg";
 ?>
 <?php include('header.php'); ?>
 
@@ -13,17 +13,11 @@
 		<div class="col-sm-12 text">
 			<div class="col-sm-4 hidden-xs"><img src="img/<?php echo $mainImage; ?>" class="img-responsive" alt=""></div>
 			<div class="col-sm-6">
-
-
 <p>The Artsida 6 vernissage will provide an opportunity for the public to meet the artists, as well as preview the collection before it goes to auction. Held at the unique and singular Galerie Dentaire on Saturday September 19<sup>th</sup> 2015, this free gallery exhibition will include additional artwork on sale exclusively for the night of the vernissage. This is a great chance to come check out the art up close and pass an enthralling evening with wine and hors d’oeuvres.</p>
 <p><strong class="mrgn-rght-md">Where:</strong> <a href="http://www.galeriedentaire.com/accueil.php">Galerie Dentaire</a>, 1200 Amherst<br />
 <strong class="mrgn-rght-md">When:</strong> Saturday, September 19<sup>th</sup> 2015<br />
 <strong class="mrgn-rght-md">Time:</strong> 5-10pm<br />
 Free entry</p>
-
-
-
-
 			</div>
 		</div>
 <?php include('footer.php');?>
