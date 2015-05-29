e0963e0a623a93c314fb6e35043ee427993d9a6a
Shawn Thompson <plansmash@me.com>
2015-05-29 13:52:48 -0400

    removed images on venue page and moved logo into paragraph

--- about-venues.php
+++ about-venues.php
@@ -14,13 +14,10 @@
 			<div class="col-sm-4 hidden-xs"><img src="img/<?php echo $mainImage; ?>" class="img-responsive" alt=""></div>
 			<div class="col-sm-6">
 <h2><a href="http://entrepotsdominion.ca/">Entrepôts Dominion</a> (main auction)</h2>
-<p>Les Entrepôts Dominion’s stunning architectural design was awarded the Special Jury Prize (Prix spécial du jury) by Créativité Montréal in 2007. Construction of the beautiful heritage building began in 1880 by the Merchants Manufacturing Company as the neighbourhood became an industrial centre in Canada with several sawmills, flourmills, tanneries and smelters. Not too far from the Atwater Market, easily accessible by metro and equipped with plenty of parking, the elegantly remodeled building makes the perfect backdrop for this major art event.</p>
-<p><a href="http://entrepotsdominion.ca/"><img src="img/venue-dominion.png" class="img-responsive" alt="Entrepôts Dominion"></a></p>
-<p><img src="img/about-accm.jpg" class="img-responsive" alt=""></p>
+<p><a href="http://entrepotsdominion.ca/"><img src="img/venue-dominion.png" class="img-responsive pull-left mrgn-rght-md" alt="Entrepôts Dominion"></a>Les Entrepôts Dominion’s stunning architectural design was awarded the Special Jury Prize (Prix spécial du jury) by Créativité Montréal in 2007. Construction of the beautiful heritage building began in 1880 by the Merchants Manufacturing Company as the neighbourhood became an industrial centre in Canada with several sawmills, flourmills, tanneries and smelters. Not too far from the Atwater Market, easily accessible by metro and equipped with plenty of parking, the elegantly remodeled building makes the perfect backdrop for this major art event.</p>
 <h2><a href="http://www.galeriedentaire.com/accueil.php">Galerie Dentaire</a> (vernissage)</h2>
-<p>Galerie Dentaire is a unique and offbeat amalgamation of an art gallery and dental clinic that has been hosting Artsida’s immensely popular art exhibitions since 2008. Situated a few steps from Sainte-Catherine’s street, it has become a focal point for Montreal art openings and cultural events.</p>
-<p class="col-xs-7"><a href="http://www.galeriedentaire.com/accueil.php"><img src="img/venue-galerie-dentaire.png" class="img-responsive" alt="Galerie Dentaire"></a></p>
-<p><img src="img/about-accm.jpg" class="img-responsive" alt=""></p>
+<p><a href="http://www.galeriedentaire.com/accueil.php" class="col-xs-4"><img src="img/venue-galerie-dentaire.png" class="img-responsive pull-left pull-left mrgn-rght-md" alt="Galerie Dentaire"></a>Galerie Dentaire is a unique and offbeat amalgamation of an art gallery and dental clinic that has been hosting Artsida’s immensely popular art exhibitions since 2008. Situated a few steps from Sainte-Catherine’s street, it has become a focal point for Montreal art openings and cultural events.</p>
+<p class="col-xs-7"></p>
 			</div>
 		</div>
 <?php include('footer.php');?>
