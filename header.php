ed8caa296f6351292c60eae3921d890ec2bfe10d
Shawn Thompson <plansmash@me.com>
2015-06-02 09:25:42 -0400

    created IE8 styles

--- header.php
+++ header.php
@@ -14,16 +14,18 @@
 	<title><?php echo $title; ?></title>
 	<!-- Fav Icon -->
 	<link rel="icon" href="favicon.ico" type="image/x-icon">
-	<!-- Bootstrap -->
-	<!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<!-- Font Awesome -->
 	<!-- <link rel="stylesheet" href="css/font-awesome.min.css"> -->
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/theme.min.css">
+	<!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<!--[if gte IE 9 | !IE ]>
+		<link rel="stylesheet" href="css/theme.min.css">
+	<![endif]-->
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="css/theme.ie.css" />
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
