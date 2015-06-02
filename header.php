4c4c9fbc73e13baf7514bab74ef641dec2bd539f
Shawn Thompson <plansmash@me.com>
2015-06-02 09:29:21 -0400

    fixed condition for IE

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-	<!--[if gte IE 9 | !IE ]>
+	<!--[if gte IE 9 | !IE ]><!-->
 		<link rel="stylesheet" href="css/theme.min.css">
 	<![endif]-->
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
