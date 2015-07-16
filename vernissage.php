cde93eac96aa8e7dc7e688facc69cd3d041511c5
Shawn Thompson <plansmash@me.com>
2015-07-15 23:24:35 -0400

    La Galerie changes

--- vernissage.php
+++ vernissage.php
@@ -7,8 +7,8 @@
 ?>
 <?php include('header.php'); ?>
 
-<p>The Artsida 6 vernissage will provide an opportunity for the public to meet the artists, as well as preview the collection before it goes to auction. Held at the unique and singular Galerie Dentaire on Saturday September 19<sup>th</sup> 2015, this free gallery exhibition will include additional artwork on sale exclusively for the night of the vernissage. This is a great chance to come check out the art up close and pass an enthralling evening with wine and hors d’oeuvres.</p>
-<p><strong class="mrgn-rght-md">Where:</strong> <a target="_blank" href="http://www.galeriedentaire.com/accueil.php">Galerie Dentaire</a>, 1200 Amherst<br />
+<p>The Artsida 6 vernissage will provide an opportunity for the public to meet the artists, as well as preview the collection before it goes to auction. Held at the unique and singular La Galerie on Saturday September 19<sup>th</sup> 2015, this free gallery exhibition will include additional artwork on sale exclusively for the night of the vernissage. This is a great chance to come check out the art up close and pass an enthralling evening with wine and hors d’oeuvres.</p>
+<p><strong class="mrgn-rght-md">Where:</strong> <a target="_blank" href="http://www.galeriedentaire.com/accueil.php">La Galerie</a>, 1200 Amherst<br />
 <strong class="mrgn-rght-md">When:</strong> Saturday, September 19<sup>th</sup> 2015<br />
 <strong class="mrgn-rght-md">Time:</strong> 5-10pm<br />
 Free entry</p>
